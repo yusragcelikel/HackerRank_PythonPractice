2427ef4c7baea2e6239e53bd22f9e7e7e1221bd5
Yusra Celikel <yusragcelikel@gmail.com>
2023-06-09 20:56:45 +0300

    practiceDay3

---
+++ problem4/loops.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+
+    if n >= 1:
+        if n <= 20:
+            i = 0
+            while i < n:
+                print(i ** 2)
+                i += 1
